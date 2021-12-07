c3d6c752f2bd55a84295e87aa37939b65d027ac6
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2021-12-08 00:20:23 +0100

    setup and header

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">  
+    <main>
+      <app-header></app-header>    
+      <router-view></router-view>
+    </main>      
+  </div>
+</template>
+
+<script>
+
+import Header from './assets/components/header.vue'
+
+export default {
+  components: {
+    'app-header': Header
+  },
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Jost', 'Segoe UI';
+  }
+  li {
+    list-style: none;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  button {
+    background-color: transparent;
+  }
+  body {
+    background-color: #1f2028;
+    color: white;
+  }
+  main {
+    width: 95%;
+    margin: auto;
+  }
+</style>
---
+++ src/assets/components/header.vue
@@ -0,0 +1,102 @@
+<template>
+    <div>
+        <header>
+            <div class="logo">Treasure A.</div>
+            <div class="menu">
+                <div class="themeToggle">
+                    <button onclick="changeTheme()" class="themeToggler"><img src="src\assets\images\sun.svg" alt="Dark Theme"></button>
+                </div>
+                <div class="burger">
+                    <router-link to="#nav">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                    </router-link>                   
+                </div>
+            </div>
+            
+        </header>
+        <nav id="nav">
+            <ul>
+                <li><router-link to="">Home</router-link></li>
+                <li><router-link to="">About</router-link></li>
+                <li><router-link to="">Projects</router-link></li>
+                <li><router-link to="">Mini Blog</router-link></li>
+                <li><router-link to="">Contact</router-link></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+<script>
+
+// theme button changing
+function changeTheme() {
+    var toggler = document.getElementsByClassName('themeToggler').value;
+    console.log (toggler)
+}
+export default {
+    
+}
+</script>
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100px;
+        width: fit-content;
+    }
+    .logo {
+        font-size: 36px;
+        font-weight: 500;
+    }
+    .menu {
+        display: flex;
+    }
+    .menu > * + * {
+        margin-left: 0.7em;
+    }
+    .themeToggle button {
+        border-radius: 50%;
+        border: 1px solid white;
+        width: 52px;
+        height: 52px;
+        line-height: 52px; 
+    }
+    .themeToggle button img {
+        content: 'assets/images/moon.svg';
+    }
+    /* burger designs */
+    .burger {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        border: 1px solid white;
+        border-radius: 50%;
+        width: 52px;
+        height: 52px;
+    }
+    .burger .line {
+        width: 22px;
+        height: 2px;
+        background-color: white;
+        margin: 5px;
+        border-radius: 8px;
+    }
+    /* navigation listings */
+    nav {
+        display: flex;
+        align-items: stretch;
+        min-height: 100%;
+    }
+    nav ul {
+        width: 100%;  
+    }
+    nav li {
+        padding-left: 1em;
+        border-top: 1px solid #ccc;
+        line-height: 70px;
+    }
+</style>
+    
