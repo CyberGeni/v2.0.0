e204822e049ff147576552de95c78311cf23bc36
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2021-12-10 12:16:05 +0100

    intro message on homepage

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 // Router imports
+import Home from './assets/components/home.vue'
 import About from './assets/components/about.vue'
 import Projects from './assets/components/projects.vue'
 import MiniBlog from './assets/components/blog.vue'
@@ -13,6 +14,7 @@ import Contact from './assets/components/contact.vue'
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes : [
+    {path: '/', component: Home},
     {path: '/blog', component: MiniBlog},
     {path: '/about', component: About},
     {path: '/projects', component: Projects},
