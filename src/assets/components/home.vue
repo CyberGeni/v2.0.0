905088ce76136e998a5908f1c311aa2941cc6fcc
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2021-12-10 19:28:52 +0100

    list change

--- src/assets/components/home.vue
+++ src/assets/components/home.vue
@@ -2,10 +2,10 @@
     <div>
         <h1>Hi. I'm Treasure. <br>Nice to meet ya.</h1>
         <p>This page is currently under development. If you were here looking for something cool, then I have two thoughts for you:</p>
-        <ol>
+        <ul>
             <li>I'm honored you'd think I could make something cool</li>
             <li>Keep checking. I update the page daily.</li>
-        </ol>
+        </ul>
     </div>
 </template>
 <script>
