3ba6dd1083c4020ab51d9cdd9c0830f53a704c22
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2021-12-11 15:57:41 +0100

    made huge progress on projects page

--- src/assets/components/featured-project.vue
+++ src/assets/components/featured-project.vue
@@ -1,21 +1,116 @@
 <template>
-    <div>
-        <main>
-            <img src="" alt="">
-            <h3></h3>
-            <p></p>
-            <div class="links">
-                <button></button>
-                <img src="/src/assets/images/info.svg" alt="">
-            </div>           
+    <div class="projects">
+        <main v-for="project in projects" :key="project.id">
+            <img :src="project.image" :alt="imageDescription" class="sitePreview">
+            <div class="info">
+                <h2 :key="project.id">{{project.title}}</h2>
+                <p>{{project.description}}</p>
+                <div class="links">
+                    <button><a :href="liveLink">Check it out</a></button>
+                    <router-link to=""><img src="/src/assets/images/info.svg" alt=""></router-link>
+                </div> 
+            </div>
+                      
         </main>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            projects: [
+                {
+                    image: '/src/assets/images/portfolio.png',
+                    title: 'devicespex',
+                    description: 'A simple software to help you spend your money wisely when choosing to buy a device. Read more...',
+                    liveLink: '',
+                    projectInfo: '',
+                    tag: 'featured',
+                    id: 2
+                },
+
+                {
+                    image: '/src/assets/images/portfolio.png',
+                    title: 'Portfolio',
+                    imageDescription: 'My portfolio site preview',
+                    description: 'A version of my portfolio website created with Vue.js. This is/was the first version that ever got publicized.',
+                    liveLink: '',
+                    projectInfo: '',
+                    tag: 'featured',
+                    id: 1
+                },
+
+                {
+                    image: '/src/assets/images/portfolio.png',
+                    title: 'LaBurtina',
+                    description: 'A responsive, lite version spice e-commerce website for a personal company.',
+                    liveLink: '',
+                    projectInfo: '',
+                    tag: 'featured',
+                    id: 2
+                }
+
+                
+            ]
+        }
+    }
 }
 </script>
 <style scoped>
-
+    .projects {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    main {
+        width: 100%;
+        max-width: 380px;
+        margin: 1em auto;
+        background-color: #2e3039;
+        padding: 10px;
+        border-radius: 12px;
+        transition: 200ms ease-in-out;
+    }
+    main:hover {
+        background-color: transparent;
+        box-shadow: 0px 0px 20px 4px #2e3039;
+        
+    }
+    main > * + * {
+        margin-right: 0.8em;
+    }
+    h2 {
+        margin: 0.5em 0;
+    }
+    .sitePreview {
+        width: 100%;
+        border-radius: 4px 4px 0 0;
+    }
+    p {
+        color: #a9adc1;
+        font-size: 1.2em;
+        font-weight: 500;
+    }
+    .info {
+        padding: 0.2em 0.7em;
+    }
+    .links {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 1em 0 0.5em 0;
+    }
+    .links button {
+        align-self: flex-end;
+        border: 1px solid #fff;
+        border-radius: 4px;
+        color: white;
+        padding: 8px 16px;
+        transition: 100ms ease-in-out;
+        font-size: 18px;
+    }
+    .links button:hover {
+        background-color: #a9adc1;
+        color: #1f2028;
+        border: 1px solid transparent;
+    }
 </style>
--- src/assets/components/home.vue
+++ src/assets/components/home.vue
@@ -8,18 +8,20 @@
                 <div class="hero-text">
                     <h1>Hi, I'm Treasure. I bring aesthetically pleasing designs to life, and create modern web solutions through code.</h1>
                     <p>Basically, I develop eye-catching websites that will definitely leave you wondering whether 'impossible' exists.</p>
-                    <h2>Learn more about me <router-link to="/about" exact><img src="src/assets/images/arrow-right.svg" alt="Arrow right button"></router-link></h2>
+                    <h2 class="see-more">Learn more about me <router-link to="/about" exact><img src="src/assets/images/arrow-right.svg" alt="Arrow right button"></router-link></h2>
                 </div>
             </div>
             <div class="projects">
+                <h1>Some of my featured projects</h1>
                 <featured></featured>
+                <h2 class="see-more">View more projects <router-link to="/projects" exact><img src="src/assets/images/arrow-right.svg" alt="Arrow right button"></router-link></h2>
             </div>
             <div class="contact"></div>
         </main>
     </div>
 </template>
 <script>
-import Featured from './project.vue'
+import Featured from './featured-project.vue'
 export default {
     components: {
         'featured': Featured
@@ -27,6 +29,7 @@ export default {
 }
 </script>
 <style scoped>
+/* hero section */
     .hero {
         display: flex;
         flex-wrap: wrap;
@@ -38,11 +41,16 @@ export default {
         margin: 2em 0;
     }
     .hero-text {
-        max-width: 400px;
+        max-width: 420px;
     }
-    .hero-text h2 {
+
+/* projects */
+    .projects h1 {
+        font-size: 1.8em;
+    }  
+    h2.see-more {
         display: flex;
-        align-content: center;
+        align-items: start;
     }
     p {
         color: #a9adc1;
@@ -50,23 +58,27 @@ export default {
         font-weight: 500;
         margin: 1em 0;
     }
-    .hero-text img {
-        margin: 0 0.5em ;
-        padding: 10px 10px;
+    h2.see-more img {
+        margin: 0 0.6em;
+        padding: 8px 8px;
         border-radius: 50%;
         border: 1px solid #a9adc1;
-        transition: 2s ease-in-out;
-        animation: slightLinearMovement infinite;
+        transition: 2s ease-in infinite;
+        animation: slightLinearMovement 1s infinite;
     }
-    .hero-text img:hover {
+    .see-more img:hover {
         border: 1px solid #fff;
     }
+    /* animations */
     @keyframes slightLinearMovement {
-        from {
-            margin: 0 0.5em;
+        0% {
+            transform: translateX(-5px);
         }
-        to {
-            margin: 0 1em ;
+        50% {
+            transform: translateX(5px);
+        }
+        100% {
+            transform: translateX(-5px);
         }
     }
     /* media queries */
@@ -74,12 +86,16 @@ export default {
     @media (min-width: 720px) {
         .hero {
             flex-grow: 1 1;
-            min-height: 80vh;
+            min-height: 85vh;
             align-content: center;
             justify-content: space-evenly;
         }
         .hero-text {
             max-width: 550px;
+        }
+        .projects {
+            align-content: center;
+            min-height: 80vh;
         }
     }
 </style>
