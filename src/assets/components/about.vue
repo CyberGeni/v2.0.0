674f7d8aa5ab78ddb23860eb41cb4776ef8cba29
Treasure <70205370+CyberGeni@users.noreply.github.com>
2022-01-25 14:24:35 +0100

    chore:  updated the about page's copy

--- src/assets/components/about.vue
+++ src/assets/components/about.vue
@@ -2,7 +2,7 @@
     <div>
         <main>
             <h1>I'm a 16 year old Software Developer based in Lagos, Nigeria.</h1>
-            <p>My software development centers mostly around Frontend Development, using HTML, CSS, TailwindCSS and Vue.js. Fun fact: I don't know JavaScript really well (right?, I'm surprised too)</p>
+            <p>My software development centers mostly around Frontend Development, using HTML, CSS, TailwindCSS and Vue.js.</p>
             <p>Other than Frontend development, I also code in Python, trying to solve some algorithmic questions.</p>
 
             <h1>Skills</h1>
@@ -10,8 +10,8 @@
             <section>
                 <h1>Now, you might be wondering... why do you need me on your team?</h1>
                     <ul>
-                        <li>If you happen to be looking for someone who can perfectly translate your designs into code, congratulations. You've found me.</li>
-                        <li>I've led many teams to success, as well as been a submissive member in other teams. Either way, I happen to be a great team player.</li>
+                        <li>Not to brag or anything :-), but I'm the person you would want to call if you wanted pixel-perfect translations of design to code. That's like... my specialty. So if that's why you're here, then hearty congratulations. You need not look further, you've got the right person for the job.</li>
+                        <li>I make a great team player. Are you searching for someone with excellent communication skills, collaboration skills, problem solving and accountability? I've got those.</li>
                         <li>It's possible that I might (<strong>respectfully</strong>) debate with you over your own project, just so it turns out excellent. In advance, sorry (not sorry). Still open  to compromises either way.</li>
                         <li>If after this you're not quite sure about how I may be of use to you, just feel free to beep me. We can have a nice chit-chat and get to know each other. You can find some of my contact infos <router-link to="contact">here</router-link>. Can't wait☺</li>
                     </ul>
