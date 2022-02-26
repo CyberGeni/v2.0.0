a82b18ef8d954c24f0c6fde9fb1a195a9038217a
Treasure <70205370+CyberGeni@users.noreply.github.com>
2022-02-26 23:58:56 +0100

    changed redirect base url

--- src/assets/components/header.vue
+++ src/assets/components/header.vue
@@ -20,7 +20,7 @@
                     <router-link to="/" ><li @click="toggled = !toggled">Home</li></router-link>
                     <router-link to="/about"><li @click="toggled = !toggled">About</li></router-link>
                     <router-link to="/projects"><li @click="toggled = !toggled">Projects</li></router-link>
-                    <router-link to="https://cybergenie.hashnode.dev"><li @click="toggled = !toggled">Blog</li></router-link>
+                    <a href="https://cybergenie.hashnode.dev"><li @click="toggled = !toggled">Blog</li></a>
                     <router-link to="/contact"><li @click="toggled = !toggled">Contact</li></router-link>
                 </ul>
             </nav>
