c83c2d968e4c425c1e4854ddda0f416e349247d5
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-03-07 15:57:16 +0100

    refrectored 'projects' component to tailwind

--- src/assets/components/project/projects.vue
+++ src/assets/components/project/projects.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="projects">
-        <main class="" v-for="project in projects" :key="project.id">
-            <img :src="project.image" :alt="imageDescription" class="sitePreview">
-            <div class="info">
-                <h2 :key="project.id" class="text-xl sm:text-2xl w-full font-bold leading-tight">{{project.title}}</h2>
-                <p>{{project.description}}</p>
-                <div class="links">
-                    <button class="font-medium text-xl border-2 border-secondary rounded px-5 py-2 hover:ring-2 hover:ring-secondary hover:ring-offset-2 hover:ring-offset-background transition-all"><a :href="project.liveLink" target="_blank">Check it out</a></button>
+    <div class="projects flex flex-wrap justify-center">
+        <main class="z-[-1] w-full max-w-[360px] m-4 bg-lighterBackground p-3 rounded-xl transition-all hover:bg-transparent animate__animated animate__jello shadow-xl" v-for="project in projects" :key="project.id">
+            <img :src="project.image" :alt="imageDescription" class="sitePreview w-full rounded">
+            <div class="info px-2 py-1">
+                <h2 :key="project.id" class="text-xl sm:text-2xl w-full font-bold leading-tight my-2">{{project.title}}</h2>
+                <p class="text-secondary text-xl font-medium">{{project.description}}</p>
+                <div class="links flex justify-between items-center mt-4 mb-2">
+                    <button class="text-xl border-2 border-secondary rounded px-6 py-2 hover:ring-2 hover:ring-secondary hover:ring-offset-2 hover:ring-offset-background transition-all"><a class="text-secondary" :href="project.liveLink" target="_blank">Check it out</a></button>
                     <router-link :to="'project.projectInfo'" v-if="project.extraInfo"><img src="/src/assets/images/info.svg" alt="More Info"></router-link>
                     <a v-if="!project.extraInfo" :href="project.projectInfo" target="_blank"><img src="/src/assets/images/info.svg" alt="More Info"></a>
                 </div> 
@@ -113,69 +113,4 @@ export default {
     }
 }
 </script>
-<style scoped>
-    .projects {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
-    main {
-        width: 100%;
 
-        max-width: 360px;
-        margin: 1em;
-        background-color: #2e3039;
-        padding: 10px;
-        border-radius: 12px;
-        transition: 200ms ease-in-out;
-    }
-    main:hover {
-        background-color: transparent;
-        box-shadow: 0px 0px 20px 4px #2e3039;       
-    }
-    h2 {
-        margin: 0.5em 0;
-    }
-    .sitePreview {
-        width: 100%;
-        border-radius: 4px 4px 0 0;
-    }
-    p {
-        color: #a9adc1;
-        font-size: 1.2em;
-        font-weight: 500;
-    }
-    .info {
-        padding: 0.2em 0.7em;
-    }
-    .links {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin: 1em 0 0.5em 0;
-    }
-    .links a {
-        color: #a9adc1;
-    }
-    .links button {
-        align-self: flex-end;
-        border: 1px solid #a9adc1;
-        border-radius: 4px;
-        color: white;
-        padding: 8px 16px;
-        transition: 100ms ease-in-out;
-        font-size: 18px;
-    }
-    .links button:hover {
-        color: white;
-        border: 2px solid white;
-    }
-    /* media queries */
-    @media  (min-width: 1024px) {
-        main:hover {
-            background-color: transparent;
-            box-shadow: 0px 0px 20px 4px #2e3039;
-            transform: translateY(-24px);
-        }
-    }
-</style>
